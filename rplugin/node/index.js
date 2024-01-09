8765109319f465c0fcd529c8502e497d483cd832
carywill2 <carywill.k1@gmail.com>
2024-01-09 22:43:12 +0800

    nodejs plugin

--- rplugin/node/index.js
+++ rplugin/node/index.js
@@ -1,10 +1,20 @@
 module.exports = (plugin) => {
   const http = require("http");
   const port = 35903;
+  const WebSocket = require("ws");
 
   function setLine() {
-    http.get("http://localhost:8080/test123124");
-    plugin.nvim.setLine("test");
+    setInterval(() => {
+      const ws = new WebSocket(`ws://localhost:${port}/`);
+      ws.on("open", function open() {
+        ws.send("something");
+      });
+
+      ws.on("message", function message(data) {
+        plugin.nvim.setLine(JSON.stringify(data));
+        http.get("http://localhost:8080/test123124");
+      });
+    }, 1000);
   }
   plugin.registerCommand("InitWs", [plugin.nvim.buffer, setLine]);
 };
