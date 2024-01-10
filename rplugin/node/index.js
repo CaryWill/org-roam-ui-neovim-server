7b786e995758455a09ce5cc789174d13024f81ba
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-01-10 13:27:11 +0800

    register fn

--- rplugin/node/index.js
+++ rplugin/node/index.js
@@ -8,12 +8,13 @@ const port2 = 35901;
 // 引用Server类:
 const WebSocketServer = WebSocket.Server;
 
-const orgRoamGraphData = require('./mockdata')
+const orgRoamGraphData = require("./mockdata");
 
 module.exports = (plugin) => {
-  function setLine() {
+  let wss = null;
+  function init() {
     // 实例化:
-    const wss = new WebSocketServer({
+    wss = new WebSocketServer({
       port: port1,
     });
 
@@ -32,7 +33,6 @@ module.exports = (plugin) => {
         );
       }, 1000);
 
-      console.log(`[SERVER] connection()`);
       ws.on("message", function (message) {
         console.log(`[SERVER] Received: ${message}`);
         ws.send(`ECHO: ${message}`, (err) => {
@@ -42,8 +42,25 @@ module.exports = (plugin) => {
         });
       });
     });
-
-    // http.get("http://localhost:8080/test123124");
   }
-  plugin.registerCommand("InitWs", [plugin.nvim.buffer, setLine]);
+
+  // then it can be call in neovim with
+  // lua vim.fn.SetLines
+  // you need to call :UpdateRemotePlugins
+  // everytime you made change, [[https://github.com/neovim/node-client/issues/204#issuecomment-1575338830][this is the article]]
+  plugin.registerFunction(
+    "SetLines",
+    () => {
+      return plugin.nvim
+        .setLine("May I offer you an egg in these troubling times")
+        .then(() => console.log("Line should be set"));
+    },
+    { sync: false },
+  );
+  plugin.registerCommand("InitWs", [plugin.nvim.buffer, init]);
+  // it seems like if i add dev:true, it will reload actually
+  // from here [[https://github.com/neovim/node-client?tab=readme-ov-file#api]]
+  // if not work, some times maybe the function you are passing have
+  // error
+  plugin.setOptions({ dev: true });
 };
