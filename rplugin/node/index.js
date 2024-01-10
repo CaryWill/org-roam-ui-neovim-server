f5a942fd468cb3af96997d980e559c4b4812feb4
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2024-01-10 10:29:41 +0800

    working

--- rplugin/node/index.js
+++ rplugin/node/index.js
@@ -1,20 +1,41 @@
+// 导入WebSocket模块:
+const WebSocket = require("ws");
+
+// ws server
+const port1 = 35903;
+// http server api
+const port2 = 35901;
+// 引用Server类:
+const WebSocketServer = WebSocket.Server;
+
 module.exports = (plugin) => {
-  const http = require("http");
-  const port = 35903;
-  const WebSocket = require("ws");
-
   function setLine() {
-    setInterval(() => {
-      const ws = new WebSocket(`ws://localhost:${port}/`);
-      ws.on("open", function open() {
-        ws.send("something");
-      });
+    // 实例化:
+    const wss = new WebSocketServer({
+      port: port1,
+    });
 
-      ws.on("message", function message(data) {
-        plugin.nvim.setLine(JSON.stringify(data));
-        http.get("http://localhost:8080/test123124");
+    wss.on("connection", function (ws) {
+      setTimeout(() => {
+        ws.send(`this is from nodejs`, (err) => {
+          if (err) {
+            console.log(`[SERVER] error: ${err}`);
+          }
+        });
+      }, 8000);
+
+      console.log(`[SERVER] connection()`);
+      ws.on("message", function (message) {
+        console.log(`[SERVER] Received: ${message}`);
+        ws.send(`ECHO: ${message}`, (err) => {
+          if (err) {
+            console.log(`[SERVER] error: ${err}`);
+          }
+        });
       });
-    }, 1000);
+    });
+
+    // http.get("http://localhost:8080/test123124");
   }
   plugin.registerCommand("InitWs", [plugin.nvim.buffer, setLine]);
 };
